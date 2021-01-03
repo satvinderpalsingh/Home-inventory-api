2ec15418bbecc6f82e4292239d8a04ce442bfb72
Satvinder Pal Singh <satvinderpalsingh52663@gmail.com>
2021-01-04 04:09:12 +0530

    connections

---
+++ backend/knexfile.js
@@ -0,0 +1,14 @@
+// Update with your config settings.
+require('dotenv').config();
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: process.env.POSTGRES_DB,
+      user:     process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD
+    },
+  }
+
+};
