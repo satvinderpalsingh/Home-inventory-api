3c6313401b59494bc92f672e98754d5ce9b93971
Satvinder Pal Singh <satvinderpalsingh52663@gmail.com>
2021-01-06 00:18:36 +0530

    seeding database and bcrypt

---
+++ backend/db/seeds/00_initial.js
@@ -0,0 +1,13 @@
+//file started with 00_initials because in order to maintain the sequence of seeding for db consistency
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('table_name').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('table_name').insert([
+        {id: 1, colName: 'rowValue1'},
+        {id: 2, colName: 'rowValue2'},
+        {id: 3, colName: 'rowValue3'}
+      ]);
+    });
+};
