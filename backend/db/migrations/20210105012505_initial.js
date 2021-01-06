6a3bcd29376706235e0ea3f6abb7e1120889dda8
Satvinder Pal Singh <satvinderpalsingh52663@gmail.com>
2021-01-07 00:41:02 +0530

    code-clean-separate-functions

--- backend/db/migrations/20210105012505_initial.js
+++ backend/db/migrations/20210105012505_initial.js
@@ -1,25 +1,11 @@
 const tableNames = require('../../src/constants/tableNames');//include to avoid to typo
-
-function addDefaultColumns(table){//as every table in a database will have the created_at ,updated_at and deleted_at column in it
-    table.timestamps(false, true);
-    table.datetime('deleted_at');
-}
-function createNameTable(knex,table_name){//for same tables in database
-    return knex.schema.createTable(table_name,(table)=>{
-        table.increments().notNullable();
-        table.string('name').notNullable().unique();
-        addDefaultColumns(table)
-    });
-}
-function references(table,table_name){
-    table.integer(`${table_name}_id`).unsigned().references('id').inTable(table_name).onDelete('cascade');//cascade take care about if we delete in foreign table then  it must be deleted in tables t is refrencing
-}
-function url(table,coloumnName){
-    table.string(coloumnName,2000);
-}
-function email(table,coloumnName){
-    return table.string(coloumnName,254);//we are returning this because in user we want it to be not nullable and unique but not in manufacturer
-}
+const {
+    addDefaultColumns,
+    createNameTable,
+    url,
+    email,
+    references,
+  }=require('../../src/lib/tableUtil');
 /**
  * @param {import('knex')} knex//adding this help us to auto complete the knex.schema.
  */
