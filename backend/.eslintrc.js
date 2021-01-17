c9d9e06d96362c3aed8ce437a1ccb1a71870d4da
Satvinder Pal Singh <satvinderpalsingh52663@gmail.com>
2021-01-18 05:15:39 +0530

    jest/supertest configured .eslintrc.js

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     browser: true,
     commonjs: true,
     es2021: true,
+    jest:true//biggest use of eslint to work with jest gloabal function
   },
   extends: [
     'airbnb-base',
