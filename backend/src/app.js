7e9038181e9cb0468f08f2b7404e520648f72c4e
Satvinder Pal Singh <satvinderpalsingh52663@gmail.com>
2021-01-18 05:00:33 +0530

    updation in files

---
+++ backend/src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
+
+const middlewares = require('./middlewares');
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(compression());
+app.use(helmet());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message:"🎄Home inventory App🎏",
+  });
+});
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+module.exports = app;
